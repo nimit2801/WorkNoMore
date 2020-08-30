cdfa72e36eed961e1cecbd14f447f1adaf440855
nimit1428 <mnimitsavant@outlook.com>
2020-08-30 18:08:49 +0530

    testing3

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+app.use(cors);
 const homePage = require("./routes/home");
 const randomTask = require("./routes/random_task");
 app.use(bodyParser.urlencoded({ extended: false }));

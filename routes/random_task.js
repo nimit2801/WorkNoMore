e4cf5f7e27faf45eb506a62cfe9217ca3c70decd
nimit1428 <mnimitsavant@outlook.com>
2020-08-30 17:54:24 +0530

    testing1

--- routes/random_task.js
+++ routes/random_task.js
@@ -1,9 +1,7 @@
 const arrPaths = [];
 arrPaths[0] = "/views/task_0.html";
 arrPaths[1] = "/views/task_1.html";
-arrPaths[2] = "/views/task_2.html";
-arrPaths[3] = "/views/task_3.html";
-arrPaths[4] = "/views/task_4.html";
+arrPaths[2] = "/views/task_3.html";
 const path = require("path");
 const rootDir = require("../utils/path");
 
